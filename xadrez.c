34e64b318ca583b8ea91869649ce11e20f3ba066
DEFIP <contatodefip@gmail.com>
2025-04-22 23:43:52 -0300

    desafio_pecas_de_xadrez_nivel_aventureiro
    Entregando o exercício do desafio xadrez nível aventureiro, onde eu melhorei a experiência do código para as peças aneriores e implementei a lógica do movimento do cavalo.

--- xadrez.c
+++ xadrez.c
@@ -2,7 +2,7 @@
 
 int main() {
     /*
-    -> Movimento de Peças de Xadrez - Nível Novato
+    -> Movimento de Peças de Xadrez - Nível Aventureiro
     -> Codado por Anderson Ferreira Fontes.
     */
 
@@ -10,6 +10,7 @@ int main() {
     
     // I. Mover a torre 5 casas para a direita - Estrutura "While".
     int t = 0; // Declarando variável de posição da torre.
+    printf("******* MOVIMENTO DA TORRE *******\n\n");
     while (t <= 5) {
         printf("Torre para a direita, da linha 0 até %d\n", t); // Imprime a posição final da torre.
         t++; // Incremento em uma casa.
@@ -18,6 +19,7 @@ int main() {
     
     
     // II. Mover bispo 5 casas para cima e para a esquerda - Estrutura "For".
+    printf("******* MOVIMENTO DO BISPO *******\n\n");
     for (int cima = 0, direita = 0; cima <= 5 && direita <= 5; cima++, direita++) { // Variáveis "cima" e "direita" declaradas internamente.
         printf("Bispo na diagonal: linha %d, coluna %d\n", cima, direita);
     }
@@ -25,6 +27,7 @@ int main() {
 
 
     // III. Mover rainha 8 casas para a esquerda - Estrutura "Do-While".
+    printf("******* MOVIMENTO DA RAINHA *******\n\n");
     int r = 8; // Declarando variável de posição da Rainha.
     do {
         printf("Rainha para a esquerda: %d\n", r); // Imprime a posição final da torre.
@@ -32,6 +35,21 @@ int main() {
     } while (r >= 0);
     printf("\n"); // Espaço para separar as peças
 
-    printf("FIM DO JOGO!");
+    // IV. Mover O cavalo Duas casas para baixo e uma para a esquerda - Estrutura "For e While" aninhadas.
+    int posicaoFinal = 3; // Declarando variável de posição do cavalo.
+    
+    printf("******* MOVIMENTO DO CAVALO *******\n\n");
+    printf("Posição inicial do cavalo -> Coluna 5, Linha 3:\n\n");
+    while (posicaoFinal > 2){
+        for (int c = 2; c > 0; c--) {
+            printf("Cavalo move para baixo na linha %d, coluna 5\n", c);
+        }
+        posicaoFinal--;
+        printf("Cavalo move para a esquerda na coluna 4, linha %d\n", posicaoFinal);
+    }
+    printf("\nPosição final do cavalo -> Coluna 4, Linha %d\n", posicaoFinal);
+    printf("\n"); // Espaço para separar as peças
+
+    printf("FIM DO JOGO!"); 
     return 0;
 }
